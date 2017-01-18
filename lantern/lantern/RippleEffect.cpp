eab0e396d876ebc491dd83d5edd636fa53ba87ac
Eric Miller <github@patternleaf.com>
2017-01-17 19:29:26 -0700

    Update gitignore. ripple effect wip.

---
+++ lantern/lantern/RippleEffect.cpp
@@ -0,0 +1,9 @@
+//
+//  RippleEffect.cpp
+//  lantern
+//
+//  Created by Eric Miller on 1/16/17.
+//  Copyright © 2017 patternleaf LLC. All rights reserved.
+//
+
+#include "RippleEffect.hpp"
---
+++ lantern/lantern/RippleEffect.hpp
@@ -0,0 +1,14 @@
+//
+//  RippleEffect.hpp
+//  lantern
+//
+//  Created by Eric Miller on 1/16/17.
+//  Copyright © 2017 patternleaf LLC. All rights reserved.
+//
+
+#ifndef RippleEffect_hpp
+#define RippleEffect_hpp
+
+#include <stdio.h>
+
+#endif /* RippleEffect_hpp */
