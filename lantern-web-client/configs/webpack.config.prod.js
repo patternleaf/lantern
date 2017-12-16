fcc82ba9b122adde5ffece0e1f41bcb173be85dc
Eric Miller <github@patternleaf.com>
2017-12-15 18:17:16 -0700

    Include less in webpack prod output.

--- lantern-web-client/configs/webpack.config.prod.js
+++ lantern-web-client/configs/webpack.config.prod.js
@@ -67,6 +67,18 @@ module.exports = {
             ['transform-object-rest-spread']
           ]
         }
+      },
+      {
+        test: /\.less$/,
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader", // translates CSS into CommonJS
+          options: { sourceMap: true }
+        }, {
+          loader: "less-loader", // compiles Less to CSS
+          options: { sourceMap: true }
+        }]
       }
     ]
   },
